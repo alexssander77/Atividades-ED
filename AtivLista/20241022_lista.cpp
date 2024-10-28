c51013e2cfe5e001826a728d0d034392d0c64fb4
Alexssander José de Oliveira de Castro <147998833+alexssander77@users.noreply.github.com>
2024-10-27 21:50:53 -0300

    Add files via upload

---
+++ AtivLista/20241022_lista.cpp
@@ -0,0 +1,238 @@
+#include<stdio.h>
+
+struct Node {
+
+    int value;
+    Node* next;
+
+    Node() { // O(1)
+        value = 0;
+        next = NULL;
+    }
+
+    Node(int _value) { // O(1)
+        value = _value;
+        next = NULL;
+    }
+
+};
+
+struct List {
+
+    Node* first;
+    Node* last;
+    int c;
+
+    List() { // O(1)
+        first = NULL;
+        last = NULL;
+        c = 0;
+    }
+
+
+    bool empty() { // O(1)
+        return first == NULL;
+    }
+
+    void pushFront(int value) { // O(1)
+        Node *n = new Node(value);
+        c++;
+        if (empty()) {
+            first = n;
+            last = n;
+            return;
+        }
+        n->next = first;
+        first = n;
+    }
+
+    void pushBack(int value) { // O(1)
+        Node *n = new Node(value);
+        c++;
+        if (empty()) {
+            first = n;
+            last = n;
+            return;
+        }
+        last->next = n;
+        last = n;
+    }
+
+    void print() { // complexidade de tempo O(n), complexidade de espaço O(1)
+        Node* aux = first;
+        while (aux != NULL) {
+            printf("%d -> ", aux->value);
+            aux = aux->next;
+        }
+    }
+
+    /*
+    int size() { // O(n)
+        int c = 0;
+        Node* aux = first;
+        while (aux != NULL) {
+            c++;
+            aux = aux->next;
+        }
+        return c;
+    }
+    */
+
+    int size() { // O(1)
+        return c;
+    }
+
+    void popFront() { // O(1)
+        if (empty()) return;
+        if (c == 1) {
+            delete(first);
+            first = NULL;
+            last = NULL;
+            c = 0;
+            return;
+        }
+        Node* toDel = first;
+        first = first->next;
+        delete(toDel);
+        c--;
+    }
+
+    void popBack() { // O(n)
+        if (empty()) return;
+        if (c == 1) {
+            delete(first);
+            first = NULL;
+            last = NULL;
+            c = 0;
+            return;
+        }
+        Node* toDel = last;
+        Node* aux = first;
+        while (aux->next != last) {
+            aux = aux->next;
+        }
+        last = aux;
+        last->next = NULL;
+        delete(toDel);
+        c--;
+    }
+
+    void insert(int value, int pos) { //O(n)
+        if (pos <= 0) {
+            pushFront(value);
+            return;
+        }
+        if (pos >= c) {
+            pushBack(value);
+            return;
+        }
+        Node* aux = first;
+        for (int i = 0; i < pos; i++, aux = aux->next);
+        Node* n = new Node(value);
+        n->next = aux->next;
+        aux->next = n;
+        c++;
+    }
+
+    void remove(int value) { // O(n)
+        if (empty()) return;
+
+        Node *aux = first;
+        Node *prev = NULL;
+        for (int i = 0; i < c; i++) {
+            if (aux->value == value) {
+                if (prev == NULL) {
+                    popFront();
+                } else {
+                    prev->next = aux->next;
+                    delete(aux);
+                    c--;
+                    aux = prev;
+                }
+            }
+            prev = aux;
+            aux = aux->next;
+        }
+    }
+
+    void removeByPos(int pos) { // O(n)
+        if (pos <= 0) {
+            popFront();
+            return;
+        }
+        if (pos >= c) {
+            popBack();
+            return;
+        }
+        Node* aux = first;
+        for (int i = 0; i < pos - 1; i++, aux = aux->next);
+        Node* toDel = aux->next;
+        aux->next = toDel->next;
+        delete(toDel);
+        c--;
+    }
+
+    void removerNBack(int n){
+        if(n>c){
+            for(int i=0;i<c;i++){
+                popBack();
+                c--;
+            }
+        }else{
+            for(int i=0;i<n;i++){
+                popBack();
+                c--;
+            }
+        }
+    }
+    void remover2(){
+        if(c<=1)return;
+        Node* aux=first->next;
+        first->next=aux->next;
+        delete(aux);
+        c--;
+    }
+    void insertCFim(){
+        pushBack(c);
+        c++;
+    }
+    void insertAteN(int n){
+        for(int i=1;i<=n;i++){
+            pushBack(i);
+            c++;
+        }
+    }
+    void insertPen(int num){
+        if(c<=1)return;
+        Node* n=new Node(num);
+        Node* aux=first;
+        while(aux->next!=last){
+            aux=aux->next;
+        }
+        aux->next=n;
+        n->next=last;
+        c++;
+
+    }
+
+};
+
+int main() {
+
+    List l;
+
+    l.pushBack(0);
+    l.pushBack(1);
+    l.pushBack(2);
+    l.pushBack(3);
+   // l.remover2();
+    //l.insertCFim();
+    l.removerNBack(2);
+    //l.insertAteN(10);
+    //l.insertPen(72);
+
+
+    l.print();
+
+    return 0;
+}
